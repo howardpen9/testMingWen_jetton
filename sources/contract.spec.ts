f87f2426b74260a4cfaa372ade4a9c50ffe19629
Howard Peng <howard.pen9@gmail.com>
2023-12-06 23:08:49 +0800

    Initial commit

---
+++ sources/contract.spec.ts
@@ -0,0 +1,149 @@
+import { ContractSystem } from "@tact-lang/emulator";
+import { buildOnchainMetadata } from "./utils/jetton-helpers";
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton-community/sandbox";
+import { beginCell, contractAddress, fromNano, StateInit, toNano } from "ton-core";
+import "@ton-community/test-utils";
+
+import { SampleJetton, Mint, TokenTransfer } from "./output/SampleJetton_SampleJetton";
+import { JettonDefaultWallet, TokenBurn } from "./output/SampleJetton_JettonDefaultWallet";
+
+//
+// This version of test is based on "@ton-community/sandbox" package
+//
+describe("contract", () => {
+    let blockchain: Blockchain;
+    let token: SandboxContract<SampleJetton>;
+    let jettonWallet: SandboxContract<JettonDefaultWallet>;
+    let deployer: SandboxContract<TreasuryContract>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        deployer = await blockchain.treasury("deployer");
+
+        // Create content Cell
+        const jettonParams = {
+            name: "Best Practice",
+            description: "This is description of Test tact jetton",
+            symbol: "XXX",
+            image: "https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn",
+        };
+        let content = buildOnchainMetadata(jettonParams);
+        let max_supply = toNano(1234766689011); // Set the specific total supply in nano
+        token = blockchain.openContract(await SampleJetton.fromInit(deployer.address, content, max_supply));
+
+        // Send Transaction
+        const deployResult = await token.send(deployer.getSender(), { value: toNano("10") }, "Mint: 100");
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: token.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it("should deploy", async () => {
+        // the check is done inside beforeEach, blockchain and token are ready to use
+        // console.log((await token.getGetJettonData()).owner);
+        // console.log((await token.getGetJettonData()).totalSupply);
+        // console.log((await token.getGetJettonData()).max_supply);
+        // console.log((await token.getGetJettonData()).content);
+    });
+
+    it("should mint successfully", async () => {
+        const player = await blockchain.treasury("player");
+        const totalSupplyBefore = (await token.getGetJettonData()).total_supply;
+        const mintAmount = toNano(100);
+        const Mint: Mint = {
+            $$type: "Mint",
+            amount: mintAmount,
+            receiver: player.address,
+        };
+        const mintResult = await token.send(deployer.getSender(), { value: toNano("10") }, Mint);
+        expect(mintResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: token.address,
+            success: true,
+        });
+
+        const totalSupplyAfter = (await token.getGetJettonData()).total_supply;
+        expect(totalSupplyBefore + mintAmount).toEqual(totalSupplyAfter);
+
+        const playerWallet = await token.getGetWalletAddress(player.address);
+        jettonWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(playerWallet));
+        const walletData = await jettonWallet.getGetWalletData();
+        expect(walletData.owner).toEqualAddress(player.address);
+        expect(walletData.balance).toEqual(mintAmount);
+    });
+
+    it("should transfer successfully", async () => {
+        const sender = await blockchain.treasury("sender");
+        const receiver = await blockchain.treasury("receiver");
+        const initMintAmount = toNano(1000);
+        const transferAmount = toNano(80);
+
+        const mintMessage: Mint = {
+            $$type: "Mint",
+            amount: initMintAmount,
+            receiver: sender.address,
+        };
+        await token.send(deployer.getSender(), { value: toNano("10") }, mintMessage);
+
+        const senderWalletAddress = await token.getGetWalletAddress(sender.address);
+        const senderWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(senderWalletAddress));
+
+        // Transfer tokens from sender's wallet to receiver's wallet
+        const transferMessage: TokenTransfer = {
+            $$type: "TokenTransfer",
+            query_id: 1n,
+            amount: transferAmount,
+            destination: receiver.address,
+            response_destination: sender.address,
+            custom_payload: null,
+            forward_ton_amount: 1n,
+            forward_payload: beginCell().endCell(),
+        };
+        const transferResult = await senderWallet.send(sender.getSender(), { value: toNano("10") }, transferMessage);
+        // console.log(transferResult.transactions);
+
+        const receiverWalletAddress = await token.getGetWalletAddress(receiver.address);
+        const receiverWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(receiverWalletAddress));
+
+        const senderWalletDataAfterTransfer = await senderWallet.getGetWalletData();
+        const receiverWalletDataAfterTransfer = await receiverWallet.getGetWalletData();
+
+        expect(senderWalletDataAfterTransfer.balance).toEqual(initMintAmount - transferAmount); // check that the sender transferred the right amount of tokens
+        expect(receiverWalletDataAfterTransfer.balance).toEqual(transferAmount); // check that the receiver received the right amount of tokens
+        // const balance1 = (await receiverWallet.getGetWalletData()).balance;
+        // console.log(fromNano(balance1));
+    });
+
+    it("Mint tokens then Burn tokens", async () => {
+        // const sender = await blockchain.treasury("sender");
+        const deployerWalletAddress = await token.getGetWalletAddress(deployer.address);
+        const deployerWallet = blockchain.openContract(JettonDefaultWallet.fromAddress(deployerWalletAddress));
+        let deployerBalanceInit = (await deployerWallet.getGetWalletData()).balance;
+        console.log("deployerBalanceInit = ", deployerBalanceInit);
+        const initMintAmount = toNano(100);
+        const mintMessage: Mint = {
+            $$type: "Mint",
+            amount: initMintAmount,
+            receiver: deployer.address,
+        };
+        await token.send(deployer.getSender(), { value: toNano("10") }, mintMessage);
+        let deployerBalance = (await deployerWallet.getGetWalletData()).balance;
+        expect(deployerBalance).toEqual(deployerBalanceInit + initMintAmount);
+
+        let burnAmount = toNano(10);
+        const burnMessage: TokenBurn = {
+            $$type: "TokenBurn",
+            query_id: 0n,
+            amount: burnAmount,
+            response_destination: deployer.address,
+            custom_payload: beginCell().endCell(),
+        };
+
+        const burnResult = await deployerWallet.send(deployer.getSender(), { value: toNano("10") }, burnMessage);
+        let deployerBalanceAfterBurn = (await deployerWallet.getGetWalletData()).balance;
+        expect(deployerBalanceAfterBurn).toEqual(deployerBalance - burnAmount);
+    });
+});
